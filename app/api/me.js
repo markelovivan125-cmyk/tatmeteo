f2ec50396e9c6cca437f23c480f327b06b9b21b0
markelovivan125-cmyk <markelovivan125@gmail.com>
2026-08-08 17:53:47 +0300

    Delete app directory

--- app/api/me.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  const cookie = req.headers.cookie || '';
-  const match = cookie.match(/auth_pass=([^;]+)/);
-  if (match) {
-    return res.json({ auth: true });
-  }
-  return res.json({ auth: false });
-}
