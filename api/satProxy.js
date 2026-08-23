79c1416c28b491b66b6cb0f80866ef42bab9190e
markelovivan125-cmyk <markelovivan125@gmail.com>
2026-08-23 18:15:40 +0300

    Update satProxy.js

--- api/satProxy.js
+++ api/satProxy.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   res.status(410).json({
     error: "External data parsing/proxy is disabled on this deployment.",
-    endpoint: "/api/nowcastProxy",
+    endpoint: "/api/satProxy",
   });
 }
